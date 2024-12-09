ae96e2f112a8348e47f56ee115a7a0ca91b2ac18
gufir <gunawan.firdaus99@ymail.com>
2024-12-09 19:59:41 +0700

    frontend: first commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import Primvue from "primevue/config";
+import router from "./router";
+import ToastService from "primevue/toastservice";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(Primvue);
+app.use(ToastService);
+
+app.mount("#app");
